99ed5308687d9c60069e484eb0c4e107329bd452
gaesigua <67262987+gaesigua@users.noreply.github.com>
2025-06-06 13:12:23 +0200

    june 6

--- src/main/java/com/gasigwatin/college_student_management_system_springboot/StudentController.java
+++ src/main/java/com/gasigwatin/college_student_management_system_springboot/StudentController.java
@@ -1,25 +1,57 @@
 package com.gasigwatin.college_student_management_system_springboot;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class StudentController {
 
     private final StudentService studentService;
+    private final StudentRepository studentRepository;
 
-    public StudentController(StudentService studentService){
+    public StudentController(StudentService studentService,
+                             StudentRepository studentRepository){
         this.studentService = studentService;
+        this.studentRepository = studentRepository;
     }
 
-//    Create new student (POST /api/students)
+//    a. Create new student (POST /api/students)
 
     @PostMapping("/api/students")
     public StudentResponseDto createStudent(
             @RequestBody StudentDto studentDto){
 
         return studentService.createStudent(studentDto);
+    }
+
+//    b. Get all students (GET /api/students) (HERE WITH EACH STUDENT'S DETAILS EXPOSED)
+
+    @GetMapping("/api/students")
+    public List<Student> retrieveAllStudents(){
+
+        return studentService.retrieveAllStudents();
+    }
+
+//    c. Get all students (Get /api/students) (HERE WITH ONLY FEW DETAILS EXPOSED)
+
+    @GetMapping("/api/students-nodetails")
+    public List<StudentResponseDto> retrieveAllStudentsWithNoDetails(){
+        return studentService.retrieveAllStudentsWithNoDetails();
+    }
+
+//    d. Get student by ID (GET /api/students/{student-id})
+
+    @GetMapping("/api/students/{student-id}")
+    public Student retrieveStudentById(@PathVariable("student-id") Integer studentId){
+        return studentService.retrieveStudentById(studentId);
+    }
+
+//    e. Get student by ID (GET /api/students-nodetails/{student-id})
+
+    @GetMapping("/api/students-nodetails/{student-id}")
+    public StudentResponseDto retrieveStudentByIdWithNoDetails(@PathVariable("student-id") Integer studentId){
+        return studentService.retrieveStudentByIdWithNoDetails(studentId);
     }
 
 }
--- src/main/java/com/gasigwatin/college_student_management_system_springboot/StudentService.java
+++ src/main/java/com/gasigwatin/college_student_management_system_springboot/StudentService.java
@@ -2,6 +2,9 @@ package com.gasigwatin.college_student_management_system_springboot;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class StudentService {
 
@@ -19,5 +22,21 @@ public class StudentService {
         var student = studentMapper.toStudent(studentDto);
         var savedStudent = studentRepository.save(student);
         return studentMapper.toStudentResponseDto(savedStudent);
+    }
+
+    public List<Student> retrieveAllStudents(){
+        return studentRepository.findAll();
+    }
+
+    public List<StudentResponseDto> retrieveAllStudentsWithNoDetails(){
+        return studentRepository.findAll().stream().map(studentMapper::toStudentResponseDto).collect(Collectors.toList());
+    }
+
+    public Student retrieveStudentById(Integer studentId){
+        return studentRepository.findById(studentId).orElse(new Student());
+    }
+
+    public StudentResponseDto retrieveStudentByIdWithNoDetails(Integer studentId){
+        return studentRepository.findById(studentId).map(studentMapper::toStudentResponseDto).orElse(null);
     }
 }
